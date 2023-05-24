5969cfdbe0f727c8340705ccd55ad02534c5a2c0
Eduardo Kuritza <81322704+ekuritza@users.noreply.github.com>
2023-05-24 20:09:09 -0300

    Update assinanteRouter.js

--- routes/assinanteRouter.js
+++ routes/assinanteRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const movieController = require('../controllers/movieController');
+const assinanteController = require('../controllers/assinanteController');
 
 router.post('/criaAssinante', assinanteController.criar);
 router.get('/assinantes', assinanteController.listar);
